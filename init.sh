4d7aebf0031e203ba61417dbc37435f9d29b40f6
Cole Ellison <cellison@squarespace.com>
2020-02-12 10:35:16 -0500

    feat: general updates to init and base profile

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 # install brew and related packages
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install ack bat dive drone git mongodb ncdu nvm parallel pyenv pipenv zsh 
+brew install ack bat drone git mongodb ncdu nvm parallel pyenv pipenv zsh kubernetes gcloud vim
 nvm install --lts
 
 # make zsh default shell
@@ -14,3 +14,7 @@ echo "source $HOME/dotfiles/.base_profile" > $HOME/.bash_profile
 echo "source $HOME/dotfiles/.zshrc" > $HOME/.zshrc
 echo "source $HOME/dotfiles/.vimrc" > $HOME/.vimrc
 cp ./.gitconfig $HOME/.gitconfig
+
+# reminders of tasks
+echo "download jetbrains mono: https://www.jetbrains.com/lp/mono/"
+echo "download go (if applicable): https://golang.org/doc/install"
